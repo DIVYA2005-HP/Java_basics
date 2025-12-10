fc4b6168a448964dccd1b548ca2d4dd0fb6b1662
Divya <maremanda.divyashree@gmail.com>
2025-12-10 15:20:47 +0530

    Add Java programs

---
+++ java_basics/j1.java
@@ -0,0 +1,22 @@
+package java_basics;
+
+public class j1 {
+    public static void main(String[] args) {
+        int x = 25;
+        double d = 25.3;
+        float f = 22.0f;
+        short s = 10;
+        long l = 123_456L;
+        char c = 'g';
+        boolean b = true;
+
+        System.out.println("Integer: " + x);
+        System.out.println("Double: " + d);
+        System.out.println("Float: " + f);
+        System.out.println("Short: " + s);
+        System.out.println("Long: " + l);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + b);
+
+    }
+}
---
+++ java_basics/j2.java
@@ -0,0 +1,10 @@
+package java_basics;
+
+public class j2 {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        System.out.println("sum:" + (x + y));
+
+    }
+}
